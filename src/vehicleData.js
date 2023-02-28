82cadfea93a71cade76ae78f0af913c40ed00691
Yanguo Cao <478218222@qq.com>
2023-02-28 17:00:44 +0000

    format the data, start developing select component

---
+++ src/vehicleData.js
@@ -0,0 +1,49 @@
+export const vehiclesData = [
+    { code: 'X123', name: 'I-Pace', modelYears: [
+            2021,
+            2022,
+            2023
+        ]
+    },
+    { code: 'X456', name: 'F-Pace', modelYears: [
+            2020,
+            2021,
+            2022,
+            2023,
+            2024
+        ]
+    },
+    { code: 'X789', name: 'F-Type', modelYears: [
+            2020,
+            2021
+        ]
+    },
+    { code: 'L123', name: 'Range Rover', modelYears: [
+            2021,
+            2022,
+            2023
+        ]
+    },
+    { code: 'L456', name: 'Range Rover Sport', modelYears: [
+            2021,
+            2022,
+            2023
+        ]
+    },
+    { code: 'X789', name: 'Range Rover Velar', modelYears: [
+            2017,
+            2018,
+            2019,
+            2020
+        ]
+    },
+    { code: 'X321', name: 'Range Rover Evoque', modelYears: [
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023
+        ]
+    },
+]
